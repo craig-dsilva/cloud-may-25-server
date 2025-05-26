d73b94cea279c45723db1ba7c2665cf6c579b256
Craig D'Silva <craig.s.dsilva@gmail.com>
2025-05-26 22:01:58 +0100

    Add secret manager config

--- index.js
+++ index.js
@@ -1,8 +1,43 @@
 import express from "express";
 import { Client } from "pg";
+import {
+  SecretsManagerClient,
+  GetSecretValueCommand,
+} from "@aws-sdk/client-secrets-manager";
+import "dotenv/config";
+
+const secret_name = "hotel-db-url";
+
+const smClient = new SecretsManagerClient({
+  region: process.env.REGION,
+  credentials: {
+    accessKeyId: process.env.ACCESS_KEY_ID,
+    secretAccessKey: process.env.SECRET_ACCESS_KEY,
+  },
+});
+
+let response;
+
+try {
+  response = await smClient.send(
+    new GetSecretValueCommand({
+      SecretId: secret_name,
+      VersionStage: "AWSCURRENT",
+    })
+  );
+} catch (error) {
+  throw error;
+}
+
+const secret = response.SecretString;
+
+console.log(secret);
 
 const app = express();
-const pgClient = new Client({});
+const pgClient = new Client({
+  connectionString: secret,
+});
+
 await pgClient
   .connect()
   .then(() => console.log("Connected to Database..."))
