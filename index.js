be437cc6f06dc1b57fb548e5c257d8f1f9619d35
Craig D'Silva <craig.s.dsilva@gmail.com>
2025-05-26 21:36:52 +0100

    Postgres

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 import express from "express";
+import { Client } from "pg";
 
 const app = express();
+const pgClient = new Client({});
+await pgClient
+  .connect()
+  .then(() => console.log("Connected to Database..."))
+  .catch((err) => console.error(`Database connection error: ${err}`));
 
 app.get("/", (req, res) => res.send("Hello World"));
 
